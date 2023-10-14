88c5e8e099c2876cf3a5e12decfdb18b8d211754
Rich Morgan <ionux@users.noreply.github.com>
2023-10-13 21:27:20 -0500

    WIP safety refactor & formatting consistency fixes

--- cores/arduino/PluggableUSB.cpp
+++ cores/arduino/PluggableUSB.cpp
@@ -28,71 +28,114 @@ extern uint8_t _initEndpoints[];
 int PluggableUSB_::getInterface(uint8_t* interfaceCount)
 {
 	int sent = 0;
-	PluggableUSBModule* node;
-	for (node = rootNode; node; node = node->next) {
-		int res = node->getInterface(interfaceCount);
-		if (res < 0)
-			return -1;
-		sent += res;
-	}
+    int res = 0;
+    
+	PluggableUSBModule* node = NULL;
+
+    if (rootNode != NULL)
+    {
+    	for (node = rootNode; node; node = node->next)
+        {
+    		res = node->getInterface(interfaceCount);
+            
+    		if (res < 0)
+            {
+    			sent = -1;
+                break;
+            }
+
+    		sent += res;
+    	}
+    }
+
 	return sent;
 }
 
 int PluggableUSB_::getDescriptor(USBSetup& setup)
 {
-	PluggableUSBModule* node;
-	for (node = rootNode; node; node = node->next) {
-		int ret = node->getDescriptor(setup);
-		// ret!=0 -> request has been processed
-		if (ret)
-			return ret;
-	}
-	return 0;
+    int res = 0;
+
+	PluggableUSBModule* node = NULL;
+
+    if (rootNode != NULL)
+    {
+    	for (node = rootNode; node; node = node->next)
+        {
+    		res = node->getDescriptor(setup);
+
+    		// res != 0 means the request has been processed
+    		if (res)
+            {
+    			break;
+            }
+    	}
+    }
+
+	return res;
 }
 
 void PluggableUSB_::getShortName(char *iSerialNum)
 {
 	PluggableUSBModule* node;
-	for (node = rootNode; node; node = node->next) {
+
+	for (node = rootNode; node; node = node->next)
+    {
 		iSerialNum += node->getShortName(iSerialNum);
 	}
+
 	*iSerialNum = 0;
 }
 
 bool PluggableUSB_::setup(USBSetup& setup)
 {
 	PluggableUSBModule* node;
-	for (node = rootNode; node; node = node->next) {
-		if (node->setup(setup)) {
+
+	for (node = rootNode; node; node = node->next)
+    {
+		if (node->setup(setup))
+        {
 			return true;
 		}
 	}
+
 	return false;
 }
 
 bool PluggableUSB_::plug(PluggableUSBModule *node)
 {
-	if ((lastEp + node->numEndpoints) > USB_ENDPOINTS) {
+	if ((lastEp + node->numEndpoints) > USB_ENDPOINTS)
+    {
 		return false;
 	}
 
-	if (!rootNode) {
+	if (!rootNode)
+    {
 		rootNode = node;
-	} else {
+	}
+    else
+    {
 		PluggableUSBModule *current = rootNode;
-		while (current->next) {
+
+		while (current->next)
+        {
 			current = current->next;
 		}
+
 		current->next = node;
 	}
 
 	node->pluggedInterface = lastIf;
 	node->pluggedEndpoint = lastEp;
+
 	lastIf += node->numInterfaces;
-	for (uint8_t i = 0; i < node->numEndpoints; i++) {
+
+	for (uint8_t i = 0; i < node->numEndpoints; i++)
+    {
 		_initEndpoints[lastEp] = node->endpointType[i];
+
 		lastEp++;
 	}
+
 	return true;
 	// restart USB layer???
 }
@@ -100,14 +143,16 @@ bool PluggableUSB_::plug(PluggableUSBModule *node)
 PluggableUSB_& PluggableUSB()
 {
 	static PluggableUSB_ obj;
+
 	return obj;
 }
 
-PluggableUSB_::PluggableUSB_() : lastIf(CDC_ACM_INTERFACE + CDC_INTERFACE_COUNT),
-                                 lastEp(CDC_FIRST_ENDPOINT + CDC_ENPOINT_COUNT),
-                                 rootNode(NULL)
+PluggableUSB_::PluggableUSB_()
+    : lastIf(CDC_ACM_INTERFACE + CDC_INTERFACE_COUNT),
+      lastEp(CDC_FIRST_ENDPOINT + CDC_ENPOINT_COUNT),
+      rootNode(NULL)
 {
-	// Empty
+	// Intentionally empty.
 }
 
 #endif
